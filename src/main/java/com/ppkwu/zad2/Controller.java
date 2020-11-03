b0c28366f5c8b6ed896b16e4e711562c6f083a8e
Zientak.David <Zenakz@github.com>
2020-11-03 14:44:44 +0100

    added a StringInfo endpoint and empty function for counting characters

--- src/main/java/com/ppkwu/zad2/Controller.java
+++ src/main/java/com/ppkwu/zad2/Controller.java
@@ -1,8 +1,19 @@
 package com.ppkwu.zad2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @GetMapping("/stringInfo")
+    public String stringInfo(@RequestBody String string){
+        String returnValue = countStringCharacters(string);
+        return returnValue;
+    }
+
+    public String countStringCharacters(String string){
+        return string;
+    }
 }
